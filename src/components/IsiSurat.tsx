fb2b36257b360dbc96ed2f8ce078c2de89b29e0c
firmankhoiril <firmankhoiril13@gmail.com>
2023-05-13 16:44:06 +0700

    fix the arab to bigger

--- src/components/IsiSurat.tsx
+++ src/components/IsiSurat.tsx
@@ -5,7 +5,7 @@ const Surat = ({ surat }: IIndexOfSurat) => {
   return (
     <Box sx={{ minHeight: 104, display: "flex", flexDirection: "column", borderBottom: 1, borderColor: "#3f3f46", gap: 2, py: 2 }}>
       <Typography variant="h5">Ayat {Math.floor(surat.id)}</Typography>
-      <Typography variant="h3">{surat.content}</Typography>
+      <Typography variant="h4">{surat.content}</Typography>
       <Typography variant="h5" sx={{ color: "#cbd5e1" }}>
         {surat.transliteration}
       </Typography>
